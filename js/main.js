1aaf14873ad8b3809b755afb5492564bfa4607e6
takeru-k <41880685+takeru-k@users.noreply.github.com>
2022-03-26 06:15:19 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+    $('.header__menu , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+  })
+});
+
+$(".openBtn").click(function () {
+    $(this).toggleClass('active');
+    $('.gnav').fadeToggle();
+});
+
+$(function(){
+	$('.toggle_title').click(function(){
+		$(this).toggleClass('selected');
+		$(this).next().slideToggle();
+	});
+});
+
+$('#check').change(function() {
+    // チェックが入っていたら有効化
+    if ( $(this).is(':checked') ){ 
+        // ボタンを有効化
+        $('#button').prop('disabled', false);
+    } else { 
+        // ボタンを無効化
+        $('#button').prop('disabled', true); 
+    }
+});
